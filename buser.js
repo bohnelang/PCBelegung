a9a25c6a65cf2505eadbef53005b5e3ac7ecac63
Andreas Bohne-Lang <bohnelang@users.noreply.github.com>
2018-02-22 10:28:24 +0100

    Add files via upload

---
+++ buser.js
@@ -0,0 +1,64 @@
+function fetch_seats(){
+	var scol = ["white","red","#EFEFEF","#8FFF8F","#CFCFCF", "#CFFFCF"];
+
+	if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            var xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var result=this.responseText;
+		var obj=JSON.parse(result);
+            for (var prop in obj) {
+			var val = obj[prop];
+			if(document.getElementById("buser"+val.ip)) { 
+				document.getElementById("buser"+val.ip).style.cssText = "background-color:"+ scol[val.css];  
+			}
+		}
+            }
+        };
+        xmlhttp.open("GET","https://www.umm.uni-heidelberg.de/bibl/pcbelegung/buserpcs.json" + "?nocache=" + (new Date()).getTime() );
+        xmlhttp.send();
+}
+
+
+function add_ids_css(){
+	var 	cssd = "width: 20px;height: 20px;border-radius: 50%;-moz-border-radius: 50%;-webkit-border-radius: 50%;background-color: white";
+
+	var 	css = '',
+    		head = document.head || document.getElementsByTagName('head')[0],
+    		style = document.createElement('style');
+
+	if( document.getElementById("buserstyle")){
+		cssd = document.getElementById("buserstyle").style.cssText ;
+	}
+
+	for(var i=1;i<255;i++) css += '#buser'+i+' {'+cssd+'}\n';
+
+
+	style.type = 'text/css';
+	if (style.styleSheet){
+  		style.styleSheet.cssText = css;
+	} else {
+  		style.appendChild(document.createTextNode(css));
+	}
+
+	head.appendChild(style);
+}
+
+
+function init(){
+	document.onreadystatechange = function () {
+  		if (document.readyState == "complete") {
+			add_ids_css();
+			fetch_seats();
+			setInterval(function(){fetch_seats(); }, 5000);
+  		}
+	}
+}
+
+
+init();
---
+++ cron.php
@@ -0,0 +1,87 @@
+#!/usr/bin/php -q 
+<?php
+
+$cls = array();
+for($i=20;$i<70;$i++){
+
+  $pid = pcntl_fork();
+  if(!$pid){
+	# Child
+	$ip="129.206.166.$i";
+	$pf=popen("ping -c 1 -W 1 $ip","r");
+	if($pf){
+		$read = trim(fread($pf, 255)); 
+		pclose($pf);
+	}
+	if(strstr($read,":")){
+		$r=0;
+		$pf=popen("PASSWD=geheim /usr/bin/net rpc registry enumerate 'HKEY_USERS' -S $ip  -U ad\\\\Administrator 2>&1 ","r");
+		if($pf){
+			while(!feof($pf)){
+				$read = trim(fread($pf, 1024));
+				if(strstr($read,"_Classes")){
+					$r = 1; 
+					break;
+				}
+				if(strstr($read,"Connection failed")){
+					$r = 2; 
+					break;
+                                }
+			}
+			if($r==0) $r = 3; 
+			pclose($pf);
+		}
+		
+	} else {
+		$r = 4;
+		$hn=gethostbyaddr($ip);
+		if( ! ($hn == $ip)){
+			$r = 5;
+		}
+	}
+
+	$w = sprintf("{\"ip\":\"%s\",\"css\":\"%s\"}",$i,$r);
+
+	$shared_id = shmop_open(getmypid(),"c",0644,strlen($w));
+	shmop_write($shared_id,$w,0);
+	shmop_close($shared_id);
+
+        exit($i);	
+
+    } else { 
+		# Parent
+		$cls[]=$pid;
+    }	
+
+}
+
+$bel=array();
+
+$je=time();
+foreach($cls as $val){
+	// Warten bis die Kind-Prozesse beendet sind 
+	while (pcntl_waitpid($val, $status) != -1) {
+		if( time()-$je>8) posix_kill($val,0); // nach 8 Sek gnadenlos killen 
+	}
+
+	// Ergebis aus dem Shared-Memory-Block holen
+	$shared_id = shmop_open($val,"a",0,0);
+       	$share_data = shmop_read($shared_id,0,shmop_size($shared_id));
+	shmop_delete($shared_id);
+	shmop_close($shared_id);
+
+	$bel[]=$share_data;
+}
+
+// Ergenis im JSON-Format schreiben. 
+$fp=fopen("/tmp/buserpcs.tmp","w");
+fputs($fp,"[");
+foreach($bel as $val){ fputs($fp,"$val,");}
+fputs($fp,"$val]");
+fclose($fp);
+
+copy("/tmp/buserpcs.tmp","/home/uniadmin/html/local/query_busers/buserpcs.json");
+
+exit(0);
+
+?>
